c0181282752c20c104bc32c95855c55952e331f9
NTHoa-NTH <dtc2154802010231@ictu.edu.vn>
2025-09-13 17:36:58 +0700

    Tạo chương trình chạy

--- CLCVd1.cs
+++ CLCVd1.cs
@@ -71,3 +71,74 @@ class Grade {
         this.score = score;
     }
 }
+
+// CHẠY CHƯƠNG TRÌNH
+// TẠO MENU LỰA CHỌN ĐỐI TƯỢNG CẦN NHẬP TỪ BÀN PHÍM
+public class CleanSchoolProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        StudentManager sm = new StudentManager();
+        TeacherManager tm = new TeacherManager();
+        CourseManager cm = new CourseManager();
+        EnrollmentManager em = new EnrollmentManager();
+        GradeManager gm = new GradeManager();
+
+        int menu = 0;
+        while (menu != 99) {
+            System.out.println("===== MENU =====");
+            System.out.println("1. Quan ly Sinh vien");
+            System.out.println("2. Quan ly Giao vien");
+            System.out.println("3. Quan ly Mon hoc");
+            System.out.println("4. Dang ky hoc");
+            System.out.println("5. Quan ly Diem");
+            System.out.println("6. Bao cao tong hop");
+            System.out.println("99. Thoat");
+            System.out.print("Chon: ");
+            menu = sc.nextInt(); sc.nextLine();
+
+            switch(menu) {
+                case 1:
+                    sm.add(new Student("SV01", "Hoa", 20, 8.5));
+                    sm.add(new Student("SV02", "Lan", 21, 7.2));
+                    sm.showAll();
+                    break;
+                case 2:
+                    tm.add(new Teacher("T01", "Thay A", "Toan"));
+                    tm.showAll();
+                    break;
+                case 3:
+                    cm.add(new Course("C01", "Lap trinh Java", 3));
+                    cm.showAll();
+                    break;
+                case 4:
+                    em.add("SV01", "C01");
+                    break;
+                case 5:
+                    gm.add("SV01", "C01", 9.0);
+                    break;
+                case 6:
+                    System.out.println("=== BAO CAO ===");
+                    for (Student s : sm.getAll()) {
+                        System.out.println("Sinh vien: " + s.name);
+                        for (Enrollment e : em.getAll()) {
+                            if (e.studentId.equals(s.id)) {
+                                for (Course c : cm.getAll()) {
+                                    if (c.id.equals(e.courseId)) {
+                                        System.out.print(" - Mon hoc: " + c.name);
+                                        for (Grade g : gm.getAll()) {
+                                            if (g.studentId.equals(s.id) && g.courseId.equals(c.id)) {
+                                                System.out.print(" | Diem: " + g.score);
+                                            }
+                                        }
+                                        System.out.println();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
